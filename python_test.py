3142a1ca3d20d18c346928edd88e30418fc9e2b5
ZMZhang <zhangzheming_zzm@foxmail.com>
2017-09-20 11:02:23 +0800

    creates python file
    The test creates the python project

---
+++ python_test.py
@@ -0,0 +1,8 @@
+# !/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# @Author :  ZZM
+# @Mail   :  zhangzhemingzzmh@gmail.com
+# @Time   :  2017/9/20 11:00
+# @File   :  python_test.py
+
+print("Hello World!")
